74ccfbea0840292d5faca9ce1fc6aad87fe087d6
Blake Knight <oss.ideas@gmail.com>
2016-11-09 21:33:28 -0600

    ✨ make starting point little more fun ✨

--- src/components/Example/index.js
+++ src/components/Example/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import styles from './style.css'
 
 export const Example = () => (
-  <div className={styles.example}>Hello, World!</div>
+  <div className={styles.example}>
+    Hello, World!
+  </div>
 )
 
 export default Example
